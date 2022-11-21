38f3d4a23964e53daefa4a418c79e3a0ca5b3e5f
Luis Gordon <luisdgordon@gmail.com>
2022-11-20 22:55:51 -0500

    Creado el objeto producto

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
 const fragment = document.createDocumentFragment()
+let car = {}
 
 document.addEventListener('DOMContentLoaded', () =>{
     fetchData()
+})
+items.addEventListener('clicl', d => {
+    addToCar(d)
 })
 
 const fetchData = async () => {
@@ -22,8 +26,30 @@ const Cards = data => {
         templateCard.querySelector('h5').textContent = element.name
         templateCard.querySelector('p').textContent = element.price
         templateCard.querySelector('img').setAttribute("src", element.img)
+        templateCard.querySelector('.btn-dark').dataset.id = element.id
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     });
     items.appendChild(fragment)
+}
+
+const addToCar = d => {
+    if (d.target.classList.contains('btn-dark')) {
+       setCar(d.target.parentElement)
+    
+    }
+    d.stopPropagation()
+}
+
+const setCar = object => {
+    const product = {
+        id: object.querySelector('.btn-dark').dataset.id,
+        name: object.querySelector('h5').textContent,
+        price: object.querySelector('p').textContent,
+        amount: 1
+    }
+    
+    if(car.hasOwnProperty(product.id)){
+        product.amount = car[product.id].amount + 1
+    }
 }
